925f20f7d01b9fd00260d2cb4da56de5f070f432
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-18 15:37:26 +0545

    needs change

---
+++ codes /binaryAdd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char bin1[50],bin2[50];
+    printf("Enter the 1st binary string:\n");
+    scanf("%s",&bin1);
+    printf("Enter the 2nd binary string:\n");
+    scanf("%s",&bin2);
+
+    int len1, len2, carry, maxSize, result, index[40];
+
+}
